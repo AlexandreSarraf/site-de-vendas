6375172925c932ec673b37a57cab4e37ef3e7984
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2024-05-07 17:12:23 -0400

    Implementada uma galeria de imagens que passam automaticamente.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+let index = 0;
+
+function nextImage() {
+    const images = document.querySelectorAll('.slide');
+    images[index].style.display = 'none';
+    index = (index + 1) % images.length;
+    images[index].style.display = 'block';
+}
+
 window.onload = function() {
     alert("Bem-vindo à Livraria Sarraf!");
+
+    const images = document.querySelectorAll('.slide');
+    setInterval(nextImage, 2000); // Troca de imagem a cada 2 segundos
 };
