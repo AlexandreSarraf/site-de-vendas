d51a013e55ddaaa56413bdfd79469b704f451512
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2024-05-09 16:02:44 -0400

    Correções no arquivo .js e html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,32 @@
-let index = 0;
-
-function nextImage() {
-    const images = document.querySelectorAll('.slide');
-    images[index].style.display = 'none';
-    index = (index + 1) % images.length;
-    images[index].style.display = 'block';
-}
-
 window.onload = function() {
     alert("Bem-vindo à Livraria Sarraf!");
 
-    const images = document.querySelectorAll('.slide');
-    setInterval(nextImage, 2000); // Troca de imagem a cada 2 segundos
 };
+
+// Array de URLs das imagens
+const images = [
+    './imagens/imagem1.jpg',
+    './imagens/imagem2.jpg',
+    './imagens/imagem3.jpg',
+    './imagens/imagem4.jpg',
+    './imagens/imagem5.jpg'
+];
+
+// Inicializa o índice do slide
+let currentIndex = 0;
+
+// Função para atualizar a imagem do slide
+function updateSlide() {
+    const slide = document.getElementById('slide');
+    slide.src = images[currentIndex]; // Define a imagem atual
+
+    // Incrementa o índice ou reinicia se chegar ao fim
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Reinicia o índice
+    }
+}
+
+// Chama a função updateSlide a cada 2 segundos (2000 milissegundos)
+setInterval(updateSlide, 2000);
