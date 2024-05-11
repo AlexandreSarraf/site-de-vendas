7e90aaa0cd68c9afdafd2b265e4426423030c810
Alexandre Sarraf <thiago-sarraf@hotmail.com>
2024-05-10 20:23:21 -0400

    Adicionado link para o Linkedin.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+/*window.onload = function() {
     alert("Bem-vindo à Livraria Sarraf!");
 
-};
+};*/
 
 // Array de URLs das imagens
 const images = [
@@ -30,3 +30,31 @@ function updateSlide() {
 
 // Chama a função updateSlide a cada 2 segundos (2000 milissegundos)
 setInterval(updateSlide, 2000);
+
+// Array de URLs das imagens para o segundo slideshow
+const images2 = [
+    '../imagens/imagem1.jpg',
+    '../imagens/imagem2.jpg',
+    '../imagens/imagem3.jpg',
+    '../imagens/imagem4.jpg',
+    '../imagens/imagem5.jpg'     // Incluí a primeira imagem no final para fazer um loop suave
+];
+
+// Inicializa o índice do segundo slideshow
+let currentIndex2 = 0;
+
+// Função para atualizar a imagem do segundo slideshow
+function updateSlide2() {
+    const slide2 = document.getElementById('slide2');
+    slide2.src = images2[currentIndex2]; // Define a imagem atual
+
+    // Incrementa o índice ou reinicia se chegar ao fim
+    if (currentIndex2 < images2.length - 1) {
+        currentIndex2++;
+    } else {
+        currentIndex2 = 0; // Reinicia o índice
+    }
+}
+
+// Chama a função updateSlide2 a cada 2 segundos (2000 milissegundos)
+setInterval(updateSlide2, 2000);
